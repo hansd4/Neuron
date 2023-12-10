d6eeed850dd1ba2616222268f0fd7816d59fb67c
Hans de los Santos <hansdls7717@gmail.com>
2023-12-09 20:14:08 -0500

    Initial Commit

---
+++ Neuron/NeuronApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuronApp.swift
+//  Neuron
+//
+//  Created by Hans de los Santos on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct NeuronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
