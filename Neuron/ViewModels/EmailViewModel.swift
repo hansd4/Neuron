26327578104150ff2e72ca3fcd26c0c9556e677e
Hans de los Santos <hansdls7717@gmail.com>
2023-12-11 09:32:05 -0500

    Improving register screen; added new screens

---
+++ Neuron/ViewModels/EmailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EmailViewModel.swift
+//  Neuron
+//
+//  Created by Hans de los Santos on 12/11/23.
+//
+
+import Foundation
---
+++ Neuron/Views/EmailView.swift
@@ -0,0 +1,18 @@
+//
+//  EmailView.swift
+//  Neuron
+//
+//  Created by Hans de los Santos on 12/11/23.
+//
+
+import SwiftUI
+
+struct EmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmailView()
+}
