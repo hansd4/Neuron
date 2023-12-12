8521d0a8f5c5f30c769a9e69340320a980afb43b
Hans de los Santos <hansdls7717@gmail.com>
2023-12-11 20:40:38 -0500

    Really, really finished login screen now -- perfect for all screen sizes, works with keyboards, fully animates

---
+++ Neuron/Views/NTextField.swift
@@ -0,0 +1,18 @@
+//
+//  NTextField.swift
+//  Neuron
+//
+//  Created by Hans de los Santos on 12/11/23.
+//
+
+import SwiftUI
+
+struct NTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NTextField()
+}
